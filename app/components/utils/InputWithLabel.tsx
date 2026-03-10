f91048db4e47b5a5446f36b69629e6f7a2c8ed2c
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-10 21:11:13 +0300

    edited input props to pass any input attr
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/utils/InputWithLabel.tsx
+++ app/components/utils/InputWithLabel.tsx
@@ -1,29 +1,12 @@
-import { ChangeEventHandler, HTMLInputTypeAttribute } from "react";
+import { ComponentPropsWithoutRef } from "react";
 import InputField from "./InputField";
 
-interface InputProps {
-  type: HTMLInputTypeAttribute;
-  label: string;
-  placeholder?: string | undefined;
-  inputClassName?: string | undefined;
+interface InputProps extends ComponentPropsWithoutRef<"input"> {
   labelClassName?: string | undefined;
-  name: string;
-  id: string;
-  value?: string | undefined;
-  onChange?(): ChangeEventHandler;
+  label: string;
 }
 
-function InputWithLabel({
-  type,
-  label,
-  inputClassName,
-  labelClassName,
-  name,
-  id,
-  placeholder,
-  value,
-  onChange,
-}: InputProps) {
+function InputWithLabel({ id, label, labelClassName, ...props }: InputProps) {
   return (
     <>
       <label
@@ -32,15 +15,7 @@ function InputWithLabel({
       >
         {label}
       </label>
-      <InputField
-        type={type}
-        id={id}
-        className={inputClassName}
-        placeholder={placeholder}
-        name={name}
-        value={value}
-        onChange={onChange}
-      />
+      <InputField id={id} {...props} />
     </>
   );
 }
