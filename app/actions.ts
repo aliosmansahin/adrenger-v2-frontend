49ec53f4567cb175058e78410a19d0c66efcb188
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-12 02:57:31 +0300

    handle signup requests
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/actions.ts
+++ app/actions.ts
@@ -55,9 +55,53 @@ export async function login(initialState: any, formData: FormData) {
 }
 
 export async function signup(initialState: any, formData: FormData) {
-    //TODO: Implement signup logic
-    return {
-        message: "test",
-        success: true,
+    const rawFormData = {
+        email: formData.get("email"),
+        password: formData.get("password"),
+        nickname: formData.get("nickname"),
+        bio: formData.get("bio"),
+        rememberMe: formData.get("remember-me"),
+    };
+
+    try {
+        const res = await axios.post(`${process.env.BACKEND_URL}/auth/register`, rawFormData, {withCredentials: true});
+
+        const cookieStore = await cookies();
+
+        cookieStore.set("access_token", res.data.access_token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            path: "/",
+            maxAge: 5 * 60,
+        });
+
+        const rawCookies = res.headers["set-cookie"]; 
+        
+        if (rawCookies) {
+            const refreshTokenCookie = rawCookies.find(c => c.startsWith("refresh_token"));
+            
+            if (refreshTokenCookie) {
+                const tokenValue = refreshTokenCookie.split(";")[0].split("=")[1];
+                
+                cookieStore.set("refresh_token", tokenValue, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "strict",
+                    maxAge: rawFormData.rememberMe ? 5 * 24 * 60 * 60 : 1 * 24 * 60 * 60,
+                    path: "/",
+                });
+            }
+        }
     }
+    catch(error: any) {
+        console.error(`An error occurred: ${error}`);
+        return {
+            message: error.response?.data?.message || "unknown_error",
+            success: false,
+        };
+    }
+
+
+    redirect("/");
 }
