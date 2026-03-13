c6f4339c8753eeb65401ebe454ead61822e7f17e
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-13 13:33:35 +0300

    edit proxy to route login when requesting protected routes while user is unauthenticated
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,17 @@
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server'
 
-export default function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL("/login", request.url));
+const protectedRoutes = ["/", "/room/:path*"];
+
+export default async function proxy(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+
+  const refresh_token = (await cookies()).get("refresh_token")?.value;
+  if(isProtectedRoute && !refresh_token)
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+
+  return NextResponse.next();
 }
 
 export const config = {
