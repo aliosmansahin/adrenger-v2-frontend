03e878d25ea9ad31add4d2c504584ac0a3a4d3c9
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-15 15:27:18 +0300

    edit button css to add disabled style, change bg-color on active not focus
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/utils/Button.tsx
+++ app/components/utils/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ComponentPropsWithoutRef<"button"> {}
 function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button
-      className={`bg-blue-500 hover:bg-blue-600 focus:bg-blue-800 hover:cursor-pointer rounded-3xl py-1 ${className}`}
+      className={`bg-blue-500 hover:bg-blue-600 active:bg-blue-800 hover:cursor-pointer disabled:bg-gray-500 disabled:cursor-default rounded-3xl py-1 ${className}`}
       {...props}
     >
       {children}
