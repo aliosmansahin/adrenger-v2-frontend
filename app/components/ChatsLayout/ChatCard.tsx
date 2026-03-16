64ce60c65ad6dfa15652a0a432ddc8c8a07e2555
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-16 14:17:22 +0300

    link to chat in chatcard; if a chat is active, disable link
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/ChatsLayout/ChatCard.tsx
+++ app/components/ChatsLayout/ChatCard.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
 export interface ChatCardData {
   roomId: number;
   role: "admin" | "member";
@@ -6,7 +9,9 @@ export interface ChatCardData {
 }
 
 function ChatCard({ roomId, role, name, lastMessage }: ChatCardData) {
-  return (
+  const { slug } = useParams();
+
+  const cardContent = (
     <div className="not-last:border-b not-last:pb-2 not-first:pt-2">
       <div className="overflow-hidden text-ellipsis">{name}</div>
       <div className="overflow-hidden text-ellipsis">
@@ -14,6 +19,21 @@ function ChatCard({ roomId, role, name, lastMessage }: ChatCardData) {
       </div>
     </div>
   );
+
+  if (roomId.toString() === slug) {
+    return (
+      <span className="bg-gray-600 rounded-4xl px-3 py-2">{cardContent}</span>
+    );
+  } else {
+    return (
+      <Link
+        href={`/room/${roomId}`}
+        className="hover:bg-gray-800 active:bg-gray-700 px-3 py-2 rounded-4xl"
+      >
+        {cardContent}
+      </Link>
+    );
+  }
 }
 
 export default ChatCard;
