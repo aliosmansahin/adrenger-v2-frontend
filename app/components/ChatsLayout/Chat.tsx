7a38551f14ef3c51401fac4bcd54bb31bb755379
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-17 21:10:10 +0300

    add message and input components
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/ChatsLayout/Chat.tsx
+++ app/components/ChatsLayout/Chat.tsx
@@ -1,10 +1,11 @@
+import MessagesWithInputContainer from "../MessagesLayout/MessagesWithInputContainer";
 import ChatHeader from "./ChatHeader";
 
 function Chat({ roomId }: { roomId: number }) {
   return (
     <div className="p-2 sm:max-w-150 sm:max-h-150 w-150 h-150">
       <ChatHeader roomId={roomId} />
-      <div>Chat</div>
+      <MessagesWithInputContainer />
     </div>
   );
 }
---
+++ app/components/MessagesLayout/MessagesWithInputContainer.tsx
@@ -0,0 +1,13 @@
+import MessageInput from "./MessageInput";
+import Messages from "./Messages";
+
+function MessagesWithInputContainer() {
+  return (
+    <div>
+      <Messages />
+      <MessageInput />
+    </div>
+  );
+}
+
+export default MessagesWithInputContainer;
