f39868c0f5c90ec201a208f6f3ec61c4c1eedd06
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-18 01:24:38 +0300

    add create room form
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/(chatslayout)/create/page.tsx
+++ app/(chatslayout)/create/page.tsx
@@ -1,9 +1,11 @@
+import CreateRoomForm from "@/app/components/CreateRoom/CreateRoomForm";
 import PageHeader from "@/app/components/Pages/PageHeader";
 
 function Page() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full flex flex-col">
       <PageHeader text="Create Room" />
+      <CreateRoomForm />
     </div>
   );
 }
