2e7b53dd73a177db13084ef0ca8c040b26ac636f
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-18 00:10:13 +0300

    add page title into create room page
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/(chatslayout)/create/page.tsx
+++ app/(chatslayout)/create/page.tsx
@@ -1,5 +1,13 @@
+import PageTitle from "@/app/components/utils/PageTitle";
+
 function Page() {
-  return <div>Create Room</div>;
+  return (
+    <div className="p-2">
+      <header className="p-3">
+        <PageTitle text="Create Room" />
+      </header>
+    </div>
+  );
 }
 
 export default Page;
