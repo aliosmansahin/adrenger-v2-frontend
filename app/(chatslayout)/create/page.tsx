9579fbf76f9b6ba68776fa86cae1fa2ec4ff447b
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-18 00:23:27 +0300

    make create page to have full parent width and height to adapt superlayout
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/(chatslayout)/create/page.tsx
+++ app/(chatslayout)/create/page.tsx
@@ -2,7 +2,7 @@ import PageTitle from "@/app/components/utils/PageTitle";
 
 function Page() {
   return (
-    <div>
+    <div className="w-full h-full">
       <header className="p-3">
         <PageTitle text="Create Room" />
       </header>
