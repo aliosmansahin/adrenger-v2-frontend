a5bf5554ce4f6b7cbe5e322ae398d62978aa6335
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-19 00:16:44 +0300

    open menu via clicking
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/ChatsLayout/ChatsHeader.tsx
+++ app/components/ChatsLayout/ChatsHeader.tsx
@@ -1,17 +1,24 @@
+"use client";
+
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import UnfilledButton from "../utils/UnfilledButton";
-import OptionMenu from "../utils/OptionMenu/OptionMenu";
+import OptionMenu, { OptionMenuHandle } from "../utils/OptionMenu/OptionMenu";
+import { useRef } from "react";
 
 function ChatsHeader() {
+  const menuRef = useRef<OptionMenuHandle>(null);
   return (
     <header className="text-[20px] h-13 px-3 border-b flex justify-between items-center">
       <h1>adrenger</h1>
       <span className="relative">
-        <UnfilledButton className="text-white">
+        <UnfilledButton
+          className="text-white"
+          onClick={() => menuRef.current?.openMenu()}
+        >
           <FontAwesomeIcon icon={faEllipsisVertical} />
         </UnfilledButton>
-        <OptionMenu />
+        <OptionMenu ref={menuRef} />
       </span>
     </header>
   );
--- app/components/utils/OptionMenu/OptionMenu.tsx
+++ app/components/utils/OptionMenu/OptionMenu.tsx
@@ -1,8 +1,35 @@
+"use client";
+
+import {
+  ComponentPropsWithRef,
+  forwardRef,
+  ReactNode,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
 import OptionMenuOption from "./OptionMenuOption";
 
-function OptionMenu() {
+export interface OptionMenuHandle {
+  openMenu: () => void;
+}
+
+interface Props {
+  children?: ReactNode;
+}
+
+const OptionMenu = forwardRef<OptionMenuHandle, Props>((props, ref) => {
+  const [show, setShow] = useState(false);
+  const menuRef = useRef(null);
+
+  useImperativeHandle(ref, () => ({
+    openMenu: () => setShow(true),
+  }));
+
+  if (!show) return null;
+
   return (
-    <div className="absolute">
+    <div className="absolute" ref={menuRef}>
       <span>Option Menu</span>
       <section>
         <OptionMenuOption />
@@ -10,6 +37,6 @@ function OptionMenu() {
       </section>
     </div>
   );
-}
+});
 
 export default OptionMenu;
