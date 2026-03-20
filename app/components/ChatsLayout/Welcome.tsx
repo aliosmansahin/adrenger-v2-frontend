6e08870567ae2c5c40c55c4bf8698638089d2824
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-20 20:34:58 +0300

    add header to welcome page to show header in mobile screens
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/ChatsLayout/Welcome.tsx
+++ app/components/ChatsLayout/Welcome.tsx
@@ -1,18 +1,24 @@
 import FilledButton from "../utils/FilledButton";
 import UnfilledLink from "../utils/UnfilledLink";
+import ChatsHeader from "./ChatsHeader";
 
 function Welcome() {
   return (
-    <div className="text-[18px] flex flex-col items-center justify-center gap-2 w-full h-full">
-      <span className="font-bold italic">Welcome to adrenger</span>
-      <span>Select a room and start chatting</span>
-      <span>or create a room and invite people</span>
-      <span>
-        <UnfilledLink href="/create" className="px-3 mr-3">
-          CREATE A ROOM
-        </UnfilledLink>
-        <FilledButton className="px-3">SETTINGS</FilledButton>
-      </span>
+    <div className="flex flex-col w-full h-full">
+      <div className="block sm:hidden">
+        <ChatsHeader />
+      </div>
+      <main className="text-[18px] flex flex-col items-center justify-center gap-2 w-full h-full">
+        <span className="font-bold italic">Welcome to adrenger</span>
+        <span>Select a room and start chatting</span>
+        <span>or create a room and invite people</span>
+        <span>
+          <UnfilledLink href="/create" className="px-3 mr-3">
+            CREATE A ROOM
+          </UnfilledLink>
+          <FilledButton className="px-3">SETTINGS</FilledButton>
+        </span>
+      </main>
     </div>
   );
 }
