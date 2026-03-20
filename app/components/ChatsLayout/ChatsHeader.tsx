bac1a99248b164938e40a9fdc10e9ab7bb8b519b
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-21 02:39:46 +0300

    add menu toggle
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/ChatsLayout/ChatsHeader.tsx
+++ app/components/ChatsLayout/ChatsHeader.tsx
@@ -5,13 +5,19 @@ import OptionMenuOption from "../utils/OptionMenu/OptionMenuOption";
 import OptionMenuWithButton from "../utils/OptionMenu/OptionMenuWithButton";
 import { MenuObject } from "../utils/OptionMenu/OptionMenu";
 import { signout } from "@/app/actions/auth";
+import MenuToggle from "../Menu/MenuToggle";
 
 function ChatsHeader() {
   const router = useRouter();
 
   return (
     <header className="text-[20px] h-13 px-3 border-b flex justify-between items-center">
-      <h1>adrenger</h1>
+      <span className="flex gap-5">
+        <span className="inline sm:hidden">
+          <MenuToggle />
+        </span>
+        <h1>adrenger</h1>
+      </span>
       <OptionMenuWithButton>
         {(menuObject: MenuObject) => (
           <>
---
+++ app/components/Menu/MenuToggle.tsx
@@ -0,0 +1,7 @@
+import UnfilledButton from "../utils/UnfilledButton";
+
+function MenuToggle() {
+  return <UnfilledButton>Menu</UnfilledButton>;
+}
+
+export default MenuToggle;
