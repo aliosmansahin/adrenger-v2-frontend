06593e10bd09b5fe58f240ff3fb9e41d69e1fd37
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-20 17:05:55 +0300

    add join form elements
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/JoinRoom/JoinRoomForm.tsx
+++ app/components/JoinRoom/JoinRoomForm.tsx
@@ -5,6 +5,9 @@ import { useQuery } from "@tanstack/react-query";
 import { ChatCardData } from "../ChatsLayout/ChatCard";
 import HintMessage from "../utils/HintMessage";
 import ErrorMessage from "../utils/ErrorMessage";
+import FilledButton from "../utils/FilledButton";
+import InputWithLabel from "../utils/InputWithLabel";
+import { SubmitEvent } from "react";
 
 export interface JoinRoomData {
   id: number;
@@ -25,13 +28,37 @@ function JoinRoomForm({ roomId }: { roomId: number }) {
     queryFn: () => fetchJoinRoom({ roomId }),
   });
 
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   let content = null;
 
   if (isLoading || !room) content = <HintMessage message="Loading Room Data" />;
   else if (error) content = <ErrorMessage message={error.message} />;
-  else content = <div className="w-full h-full"></div>;
+  else
+    content = (
+      <form onSubmit={handleSubmit}>
+        <div className="w-full h-full flex flex-col gap-2">
+          <span>
+            <span className="italic">Join room: </span>
+            {room.name}
+          </span>
+          {!room.hasPassword && (
+            <InputWithLabel
+              label="Room Password"
+              placeholder="Enter room password"
+              name="room-password"
+              id="room-password-input"
+              type="password"
+            />
+          )}
+          <FilledButton type="submit">JOIN ROOM</FilledButton>
+        </div>
+      </form>
+    );
 
-  return <main className="w-full grow text-[18px]">{content}</main>;
+  return <main className="w-full grow text-[18px] mt-3">{content}</main>;
 }
 
 export default JoinRoomForm;
