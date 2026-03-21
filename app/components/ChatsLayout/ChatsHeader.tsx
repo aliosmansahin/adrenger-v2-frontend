708cf595e1934573dc764a8d501ce219e8534931
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-21 03:55:42 +0300

    make app title link to welcome page, when its clicked, close the menu
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/ChatsLayout/ChatsHeader.tsx
+++ app/components/ChatsLayout/ChatsHeader.tsx
@@ -6,9 +6,13 @@ import OptionMenuWithButton from "../utils/OptionMenu/OptionMenuWithButton";
 import { MenuObject } from "../utils/OptionMenu/OptionMenu";
 import { signout } from "@/app/actions/auth";
 import MenuToggle from "../Menu/MenuToggle";
+import UnfilledLink from "../utils/UnfilledLink";
+import { useMenu } from "@/app/context/MenuContext";
 
 function ChatsHeader() {
   const router = useRouter();
+
+  const { menuOpen, toggleMenu } = useMenu();
 
   return (
     <header className="text-[20px] h-13 px-3 border-b flex justify-between items-center">
@@ -16,7 +20,15 @@ function ChatsHeader() {
         <span className="inline sm:hidden">
           <MenuToggle />
         </span>
-        <h1>adrenger</h1>
+        <UnfilledLink
+          href="/"
+          className="text-white"
+          onClick={() => {
+            if (menuOpen) toggleMenu();
+          }}
+        >
+          <h1>adrenger</h1>
+        </UnfilledLink>
       </span>
       <OptionMenuWithButton>
         {(menuObject: MenuObject) => (
--- app/components/utils/UnfilledLink.tsx
+++ app/components/utils/UnfilledLink.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
 interface LinkProps {
   href: string;
   className?: string | undefined;
   children: ReactNode;
+  onClick?: MouseEventHandler;
 }
 
-function UnfilledLink({ href, className, children }: LinkProps) {
+function UnfilledLink({ href, className, children, onClick }: LinkProps) {
   return (
     <Link
       href={href}
       className={`text-blue-400 hover:text-blue-500 active:text-blue-700 ${className}`}
+      onClick={onClick}
     >
       {children}
     </Link>
