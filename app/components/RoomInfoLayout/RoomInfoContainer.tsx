2a768ab5cb480e69f9788d8d745fb0294073c483
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-23 20:16:19 +0300

    switch edit and info page
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/RoomInfoLayout/RoomInfoContainer.tsx
+++ app/components/RoomInfoLayout/RoomInfoContainer.tsx
@@ -5,8 +5,13 @@ import { ChatData } from "../ChatsLayout/ChatHeader";
 import { useQuery } from "@tanstack/react-query";
 import HintMessage from "../utils/HintMessage";
 import ErrorMessage from "../utils/ErrorMessage";
+import UnfilledButton from "../utils/UnfilledButton";
+import { useState } from "react";
 
 function RoomInfoContainer({ roomId }: { roomId: number }) {
+  const [editOpened, setEditOpened] = useState(false);
+  const [showSaveButton, setShowSaveButton] = useState(false);
+
   const {
     data: room,
     isLoading,
@@ -23,18 +28,47 @@ function RoomInfoContainer({ roomId }: { roomId: number }) {
   else
     content = (
       <div className="flex flex-col gap-3 [&>span>span]:font-bold [&>span>span]:italic">
-        <span>
-          <span>Room Name:</span> {room.name}
-        </span>
-        <span>
-          <span>Your Role:</span> {room.role}
-        </span>
-        <span>
-          <span>Room creator:</span> {room.createdBy.nickname}
-        </span>
-        <span>
-          <span>Create time:</span> {room.createdAt.toLocaleString()}
-        </span>
+        {editOpened ? (
+          <>
+            <div className="flex [&>button]:p-3 gap-2">
+              {showSaveButton && (
+                <UnfilledButton
+                  className="text-start"
+                  onClick={() => setEditOpened(false)}
+                >
+                  Save
+                </UnfilledButton>
+              )}
+              <UnfilledButton
+                className="text-start"
+                onClick={() => setEditOpened(false)}
+              >
+                Cancel
+              </UnfilledButton>
+            </div>
+          </>
+        ) : (
+          <>
+            <UnfilledButton
+              className="text-start p-3"
+              onClick={() => setEditOpened(true)}
+            >
+              Edit Room
+            </UnfilledButton>
+            <span>
+              <span>Room Name:</span> {room.name}
+            </span>
+            <span>
+              <span>Your Role:</span> {room.role}
+            </span>
+            <span>
+              <span>Room creator:</span> {room.createdBy.nickname}
+            </span>
+            <span>
+              <span>Create time:</span> {room.createdAt.toLocaleString()}
+            </span>
+          </>
+        )}
       </div>
     );
 
