3861947043dd6de7d52a680622a5ce58fc1c5236
aliosmansahin <aliosmsahin01@gmail.com>
2026-03-23 22:00:45 +0300

    update text color instead of bg color of disabled state of unfilled button
    Signed-off-by: aliosmansahin <aliosmsahin01@gmail.com>

--- app/components/utils/UnfilledButton.tsx
+++ app/components/utils/UnfilledButton.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ComponentPropsWithoutRef<"button"> {}
 function UnfilledButton({ children, className, ...props }: ButtonProps) {
   return (
     <button
-      className={`text-blue-400 hover:text-blue-500 hover:cursor-pointer active:text-blue-700 disabled:bg-gray-500 disabled:cursor-default ${className}`}
+      className={`text-blue-400 hover:text-blue-500 hover:cursor-pointer active:text-blue-700 disabled:text-gray-400 disabled:cursor-default ${className}`}
       {...props}
     >
       {children}
